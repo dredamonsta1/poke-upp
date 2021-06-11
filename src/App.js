ac66b97b6eea80cc01811e73fe4c57d93e2c0083
andre <andre_w3@yahoo.com>
2021-06-10 22:44:57 -0400

    added folder to src and pokefetch component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
+import PokeFetch from './components/PokeFetch'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Hello</h1>
+      <h1>POKE UPP</h1>
+
+      <PokeFetch/>
         <img src={logo} className="App-logo" alt="logo" />
         
         
---
+++ src/components/PokeFetch.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+
+const PokeFetch = () => {
+
+
+
+
+
+    
+    return(
+        <h2>place pokemon here</h2>
+    )
+
+}
+
+
+
+
+
+
+export default PokeFetch;
