2c7df135eb67c86d86f4dac4fed592dd8c06ac45
andre <andre_w3@yahoo.com>
2021-06-11 02:16:47 -0400

    added http

--- src/components/PokeFetch.js
+++ src/components/PokeFetch.js
@@ -4,11 +4,13 @@ import React from 'react';
 
 const PokeFetch = () => {
 
+    const poke = fetch('https://pokeapi.co/api/v2/')
 
 
 
 
-    
+
+
     return(
         <h2>place pokemon here</h2>
     )
